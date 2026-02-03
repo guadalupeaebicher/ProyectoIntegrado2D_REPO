015ac5a6d926721f3ce93eb239281f712b6ff779
JuanCarlosHerediaJimenez <juancadev11@gmail.com>
2026-02-03 11:35:00 +0100

    test playercontroller

---
+++ ProyectoIntegrado2D_Project/Assets/PI2D_Root/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerCOntroller : MonoBehaviour
+{
+    private bool isFacingRight = true;
+    private float horizontal;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
